e13afc7e645939607de444cc61eaa84c7b87ec2d
unknown <pehlivanovicalmir1@gmail.com>
2022-02-08 10:10:10 +0100

    Login function

--- nuxt/pages/auth/login.vue
+++ nuxt/pages/auth/login.vue
@@ -5,15 +5,15 @@
                 <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
                 <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
             </div>
-            <form class="mt-8 space-y-6" action="#" method="POST">
+            <form @submit.prevent="login" class="mt-8 space-y-6" action="#" method="POST">
                 <div class="rounded-md shadow-sm -space-y-px">
                     <div>
                         <label for="email-address" class="sr-only">Email address</label>
-                        <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                        <input id="email-address" v-model="form.email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
                     </div>
                     <div>
                         <label for="password" class="sr-only">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+                        <input id="password" v-model="form.password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
                     </div>
                 </div>
                 <div>
@@ -28,6 +28,24 @@
 
 <script>
 export default {
-     layout: 'base',
+    layout: 'base',
+    data(){
+        return{
+            form: {
+                email: '',
+                password: '',
+            }
+        }
+    },
+    methods: {
+        async login(){
+           try {
+            let response = await this.$auth.loginWith('local', { data: this.form })
+                console.log(response)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
 }
 </script>
