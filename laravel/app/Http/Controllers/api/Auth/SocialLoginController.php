d3dc8f140007122d171d05bf56ea2c04179f987a
unknown <pehlivanovicalmir1@gmail.com>
2022-02-08 12:58:36 +0100

    Save social logins to database

--- laravel/app/Http/Controllers/api/Auth/SocialLoginController.php
+++ laravel/app/Http/Controllers/api/Auth/SocialLoginController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers\api\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use Tymon\JWTAuth\JWTAuth;
+use Laravel\Socialite\Two\InvalidStateException;
+use App\Models\User;
+use App\Models\UserSocial;
 
 class SocialLoginController extends Controller
 {
-    public function __construct()
+    protected $auth;
+
+    public function __construct(JWTAuth $auth)
     {
+        $this->auth = $auth;
         $this->middleware('social');
     }
 
@@ -20,8 +27,61 @@ class SocialLoginController extends Controller
 
     public function callback($service)
     {
+        try{
+
+        }catch (InvalidStateException $e){
+            return redirect(env('CLIENT_BASE_URL') . '?error=Unable to login using '. $service . '. Please try again.');
+        } 
+
         $serviceUser = Socialite::driver($service)->stateless()->user();
 
-        dd($serviceUser);
+        $email = $serviceUser->getEmail();
+        if($service != 'google'){
+            $email = $serviceUser->getId() . '@' . $service . '.local';
+        }
+
+        $user = $this->getExistingUser($serviceUser, $email, $service);
+        if(!$user)
+        {
+            $user = User::create([
+                'name' => $serviceUser->getName(),
+                'email' => $email,
+                'password' => ''
+            ]);
+        }
+
+        if($this->needsToCreateSocial($user, $service)){
+            UserSocial::create([
+                'user_id'   => $user->id,
+                'social_id' => $serviceUser->getId(),
+                'service'   => $service
+            ]);
+        }
+
+        return redirect(env('CLIENT_BASE_URL') . '/auth/social-callback?token=' . $this->auth->fromUser($user));
+
+    }
+
+    public function needsToCreateSocial(User $user, $service)
+    {
+        // From modal
+        return !$user->hasSocialLinked($service);
+    }
+
+    public function getExistingUser($serviceUser, $email, $service)
+    {
+        if($service == 'google')
+        {
+            return User::where('email', $email)
+                        ->orWhereHas('social', function($q) use ($serviceUser, $service) {
+                                $q->where('social_id', $serviceUser->getId())
+                                  ->where('service', $service);
+            })->first();
+        }
+        else
+        {
+            $userSocial = UserSocial::where('social_id', $serviceUser->getId())->first();
+            return $userSocial ? $userSocial->user : null;
+        }
     }
 }
